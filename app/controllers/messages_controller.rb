687483a9100311148068e60ba52e3b8608784769
Ryo Masuda <mrra4611@gmail.com>
2017-04-01 12:09:41 +0900

    Set variables in massages/index.html.haml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,10 @@
 class MessagesController < ApplicationController
   def index
+    if params[:group_id].present?
+      @group = Group.find(params[:group_id])
+    else
+      @group = current_user.groups.first
+    end
+    @groups = current_user.groups
   end
 end
