46140f70ae210745c0f5a714d606342780547de6
Ryo Masuda <mrra4611@gmail.com>
2017-04-03 13:35:13 +0900

    Test validation and association in spec/models/group_spec.rb

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Group, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Group do
+  describe 'validation' do
+    it 'should be valid' do
+      group = build(:group)
+      expect(group).to be_valid
+    end
+    it 'should not be valid' do
+      group = build(:group, name: '')
+      expect(group).to_not be_valid
+    end
+  end
+  describe 'association' do
+    let(:group){ create(:group) }
+    let(:user_group){ create(:user_group) }
+    it 'is deleted with massages ' do
+      message = create(:message, group: group)
+      expect{ group.destroy }.to change{ Message.count }
+    end
+    it 'has many users' do
+      users = build_list(:user, 3)
+      group = create(:group, users: users)
+      expect(group.users).to eq users
+    end
+  end
 end
